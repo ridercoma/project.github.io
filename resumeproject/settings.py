91dae3f9f7ade26560ee7b27857d7f4c474abeb6
ridercoma <65594944+ridercoma@users.noreply.github.com>
2022-03-28 20:47:47 +0530

    Update settings.py

--- resumeproject/settings.py
+++ resumeproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vs=^8fw$xb84(p8ail_r_++&(r--ccn*cgr^40pa2x=y&e7goh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
